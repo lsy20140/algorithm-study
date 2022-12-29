19a17f944656699abb7647fac955d71ae098c943
lsy20140 <lsy20140@naver.com>
2022-12-29 23:50:19 +0900

    Lv1_문자열을 정수로 바꾸기

---
+++ programmers-js/Lv1_문자열을 정수로 바꾸기.js
@@ -0,0 +1,10 @@
+function solution(s) {
+  return Number(s);
+
+  // str을 num으로 자동 인식..
+  //return str/1
+}
+
+//test
+console.log("결과1: "+ solution("1234"));
+console.log("결과1 type" + typeof solution("1234"));
