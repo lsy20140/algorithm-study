d50a337675d02f387849dd44863eb57a5abe5bd1
lsy20140 <lsy20140@naver.com>
2023-05-13 22:04:52 +0900

    Lv2_예상 대진표

---
+++ programmers-js/Lv2/Lv2_예상 대진표.js
@@ -0,0 +1,16 @@
+function solution(n,a,b)
+{    
+    let answer = 0;
+    
+    while(a !== b){
+        a = Math.ceil(a/2)
+        b = Math.ceil(b/2)
+        answer++
+    }
+    
+    return answer;
+    
+}
+
+//test
+console.log("결과1 "+ solution(8, 4, 7));
